cf51e90f0ff541f1487902a6627ea84c796e1aba
Ioná <ionasantana123@gmail.com>
2019-08-03 16:57:20 -0300

    ObjetoNormal

---
+++ Assets/Scripts/ObjetoNormal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetoNormal : MonoBehaviour
+{
+    public float speed;
+    public GameObject monstro;
+    public GameObject player;
+    public Transform tr;
+    public Transform trPlayer;
+
+    public bool andaMonstro;
+    void Start()
+    {
+        tr = monstro.GetComponent<Transform>();
+        trPlayer = monstro.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if(andaMonstro == true)
+        {
+            //anda monstro
+            Vector3 movement = new Vector3(-(0.2f) * speed,0f,0f);
+            tr.position += movement; 
+
+            //anda player
+            trPlayer.position += movement;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other) 
+    {
+       if(other.gameObject.name.Equals("Player"))
+       {
+           monstro.SetActive(true);
+           andaMonstro = true;     
+           player.GetComponent<Mover>().enabled = false;     
+       }
+
+    }
+}
