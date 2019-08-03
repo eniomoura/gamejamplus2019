42501f5d5c251183228913b3101f077b7707faa9
Enio <mandosaldmer@gmail.com>
2019-08-03 18:19:09 -0300

    Incluindo Endless Runner (fase adjacente)

--- Assets/GenerateEndlessRunner.cs
+++ Assets/GenerateEndlessRunner.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class GenerateEndlessRunner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] jumpables;
+    public float firstX;
+    public float minDistanceBetween;
+    public float maxDistanceBetween;
+    public float distanceToGenerate;
+
     void Start()
     {
-        
+        float lastX = firstX;
+        Debug.Log(distanceToGenerate-maxDistanceBetween);
+        for(int i=0;lastX<firstX+distanceToGenerate-maxDistanceBetween;i++){
+            int choosenJumpableIndex = Random.Range(0, jumpables.Length);
+            GameObject choosenJumpable = jumpables[choosenJumpableIndex];
+            int choosenJumpableXPosition = (int) Mathf.Ceil(
+                lastX+Random.Range(minDistanceBetween, maxDistanceBetween));
+            Debug.Log(choosenJumpableXPosition);
+            Instantiate(choosenJumpable,
+                new Vector3(
+                    choosenJumpableXPosition,
+                    choosenJumpable.transform.position.y,
+                    choosenJumpable.transform.position.z),
+                Quaternion.identity);
+            lastX = choosenJumpableXPosition;
+        }
     }
 }
