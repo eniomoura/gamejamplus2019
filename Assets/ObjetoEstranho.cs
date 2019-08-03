c90c49ad130851fe2a4568f7218ee56f63876430
Dyogo Koithi Okada <koithiokada@gmail.com>
2019-08-03 19:37:38 -0300

    Revert "PublicLocalTeleport"
    This reverts commit c6922efec0fe1cbde73283d2277bb2f74fe4ca8c.

--- Assets/ObjetoEstranho.cs
+++ Assets/ObjetoEstranho.cs
@@ -7,8 +7,8 @@ public class ObjetoEstranho : MonoBehaviour
     public GameObject player;
     public float startDistance;
     Transform translate;
-    public Vector3 local;
-
+    
+    
     void Start()
     {
         translate = player.GetComponent<Transform>();
@@ -17,8 +17,10 @@ public class ObjetoEstranho : MonoBehaviour
 
     void OnTriggerEnter(Collider Colisao){
         if(Colisao.gameObject.name == "Player"){
-           
-            translate.position += local;
+            translate.position = new Vector3(
+                translate.position.x+500,
+                translate.position.y, translate.position.z);
+            player.GetComponent<Mover>().speed=0.4f;
         }
     }
 }
