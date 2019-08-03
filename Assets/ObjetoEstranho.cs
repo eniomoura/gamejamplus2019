dfddae74baad9f89cf82e16283ca5ba23f945545
Dyogo Koithi Okada <koithiokada@gmail.com>
2019-08-03 17:12:27 -0300

    ObjetoEstranho

---
+++ Assets/ObjetoEstranho.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEstranho : MonoBehaviour
+{
+    public GameObject player;
+    Transform translate;
+    
+    void Start()
+    {
+        translate = player.GetComponent<Transform>();
+        print(translate);
+    }
+
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider Colisao){
+        if(Colisao.gameObject.name == "Player"){
+           
+            translate.position += new Vector3 ( 0.0f, 20.0f , 0.0f);
+        }
+    }
+}
