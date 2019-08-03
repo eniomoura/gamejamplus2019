5265583892f9bef9e37f49eb1dc010c634d51895
Enio <mandosaldmer@gmail.com>
2019-08-03 16:09:35 -0300

    Resolvendo problemas com pulo

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -6,7 +6,6 @@ public class Mover : MonoBehaviour
 {
     public float speed;
     public float jumpStrength;
-    public bool isJumping;
     public Transform tr;
     public Rigidbody rb;
 
@@ -14,21 +13,20 @@ public class Mover : MonoBehaviour
     {
         tr=GetComponent<Transform>();
         rb=GetComponent<Rigidbody>();
+        rb.sleepThreshold=0;
     }
 
     void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.Space)&&!isJumping){
-            isJumping = true;
-            rb.AddForce(new Vector3(0f, jumpStrength, 0f));
-        }
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed,0f,0f);
         tr.position += movement;
     }
 
-    public void OnCollisionEnter(Collision other) {
-        if(other.gameObject.tag=="Terrain"){
-            isJumping = false;
+    public void OnCollisionStay(Collision other) {
+        Debug.Log("Collided");
+        if(Input.GetAxis("Jump")!=0){
+            Debug.Log("Jumped");
+            rb.velocity+=new Vector3(0f, jumpStrength, 0f);
         }
     }
 }
