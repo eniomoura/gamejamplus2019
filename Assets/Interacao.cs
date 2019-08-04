32c66dc96e0c64a16654bf48f463bd5b755bed95
Enio <mandosaldmer@gmail.com>
2019-08-04 02:09:45 -0300

    Corrigindo ordem dos interagíveis

--- Assets/Interacao.cs
+++ Assets/Interacao.cs
@@ -22,25 +22,24 @@ public class Interacao : MonoBehaviour
     {
        if(other.gameObject.tag.Equals("Normal"))
        {
-           if(Input.GetKey(KeyCode.E))
-           {
-               monstro.GetComponent<Transform>().position = new Vector3(
-               transformPlayer.position.x+monsterStartDistance,1,0f);
-               monstro.SetActive(true);
-               GameObject[] lareiras = GameObject.FindGameObjectsWithTag("Respawn");
-               player.GetComponent<Mover>().enabled = false;
-               Correr pc = player.GetComponent<Correr>();
-               pc.direction = -1;
-               pc.enabled = true;
-               Correr mc = monstro.GetComponent<Correr>();
-               mc.direction = -1;
-               mc.enabled = true;
-           }
+            monstro.GetComponent<Transform>().position = new Vector3(
+            transformPlayer.position.x+monsterStartDistance,1,0f);
+            monstro.SetActive(true);
+            GameObject[] lareiras = GameObject.FindGameObjectsWithTag("Respawn");
+            player.GetComponent<Mover>().enabled = false;
+            Correr pc = player.GetComponent<Correr>();
+            pc.direction = -1;
+            pc.enabled = true;
+            Correr mc = monstro.GetComponent<Correr>();
+            mc.direction = -1;
+            mc.enabled = true;
        }
     }   
 
     public void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag.Equals("Estranho")){
+           if(Input.GetKey(KeyCode.E))
+           {
             Vector3 runnerStartPosition = new Vector3(
                 transformPlayer.position.x+500,
                 transformPlayer.position.y,
@@ -52,6 +51,7 @@ public class Interacao : MonoBehaviour
                 transformPlayer.position.x-monsterStartDistance,1,0f);
             player.GetComponent<Death>().respawnPoint=runnerStartPosition;
             monstro.SetActive(true);
+           }
         }
     }
 }
